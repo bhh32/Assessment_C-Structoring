4ca08d8b30d504fb8792137babcb56f06029e626
Bryan Hyland <bryan.hyland32@gmail.com>
2018-04-10 08:04:38 -0700

    PlayerMovement Changed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,23 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] float speed = 10f;
-    [SerializeField] float rotate = 20f;
-    [SerializeField] float turnVal = 40f;
 	
 	// Update is called once per frame
 	void Update () 
     {
         // Direction variables
         float moveForward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float moveRight = Input.GetAxis("Horizontal") * turnVal * .02f;
-        float rotateVal = Input.GetAxis("Horizontal") * rotate * Time.deltaTime;
+        float moveRight = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
         // Movement Vector
-        Vector3 move = new Vector3(0f, moveRight, moveForward);
-        Vector3 turn = new Vector3(0f, 0f, -rotateVal);
+        Vector3 move = new Vector3(moveRight, 0f, moveForward);
 
-        // Applied Force
-        rb.AddForce(move);
-        rb.AddTorque(turn);
+        transform.Translate(move);
 	}
 }
