29f6d273457cf946fa44954ccfb6a4aad6049037
Bryan Hyland <bryan.hyland32@gmail.com>
2018-04-11 12:05:14 -0700

    Add XInput and Tweaked Controls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
 
 public class PlayerMovement : MonoBehaviour 
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] float speed = 10f;
-	
+	[SerializeField] float pitchSpeed = 10f;
+	[SerializeField] float yawSpeed = 10f;
+	PlayerIndex playerCont;
+	GamePadState controllerState;
+
+	void Awake()
+	{
+		playerCont = PlayerIndex.One;
+		rb.inertiaTensor = new Vector3(1, 1, 1);
+	}
+
 	// Update is called once per frame
 	void Update () 
     {
-        // Direction variables
-        float moveForward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float moveRight = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		controllerState = GamePad.GetState (playerCont);
 
-        // Movement Vector
-        Vector3 move = new Vector3(moveRight, 0f, moveForward);
+		if (controllerState.IsConnected) 
+		{
+			
 
-        transform.Translate(move);
+			if (controllerState.Buttons.A == ButtonState.Pressed) 
+				rb.AddRelativeForce (0f, 0f, -speed * Time.deltaTime);
+			else if (controllerState.Buttons.B == ButtonState.Pressed && rb.velocity.z > 0f) 
+				rb.AddRelativeForce(0f, 0f, speed * Time.deltaTime);
+
+			if (controllerState.ThumbSticks.Left.X > .5f)
+				rb.AddRelativeTorque(0f, 0f, yawSpeed * Time.deltaTime, ForceMode.Force);
+			else if (controllerState.ThumbSticks.Left.X < -.5f)
+				rb.AddRelativeTorque(0f, 0f, -yawSpeed * Time.deltaTime, ForceMode.Force);
+
+			if (controllerState.ThumbSticks.Left.Y > .5f)
+				rb.AddRelativeTorque(-pitchSpeed * Time.deltaTime, 0f, 0f, ForceMode.Force);
+			else if (controllerState.ThumbSticks.Left.Y < -.5f)
+				rb.AddRelativeTorque(pitchSpeed * Time.deltaTime,0f, 0f, ForceMode.Force);
+
+		}
 	}
 }
